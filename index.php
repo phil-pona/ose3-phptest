10851873bc5b9be142295b72610f8fb613f0549b
Thomas Philipona <philipona@puzzle.ch>
2016-01-28 13:51:13 +0100

    updated to Meetup Example

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
-<?php
-echo 'Test';
-phpinfo();
-?>
+<!DOCTYPE html>
+<html>
+   <head>
+     <title>OpenShift Meetup in Bern (28.1.2016)</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <!-- Bootstrap -->
+      <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+      </head>
+      <body>
+        <h1>Hallo <?php echo 'B&auml;rn'?>!!!</h1>
+        <script src="http://code.jquery.com/jquery.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+      </body>
+    </html>
