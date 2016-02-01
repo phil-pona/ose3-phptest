da2d73546f50f6d34c57e787036cd9b7a15d8f34
Thomas Philipona <philipona@puzzle.ch>
2016-02-01 21:39:08 +0100

    updated date

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">OpenShift Demo 28.1.2016</a>
+          <a class="navbar-brand" href="#">OpenShift Demo 2.2.2016</a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
